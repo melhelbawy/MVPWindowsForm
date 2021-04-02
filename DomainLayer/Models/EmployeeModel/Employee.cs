4f35d516250c44b4f9605f6bc5400389bc20f8c0
Mohamed Mohamed Elhelbawy <m_elhelbawy@outlook.com>
2021-04-02 19:01:23 +0200

    Add Employye Model

---
+++ DomainLayer/Models/EmployeeModel/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.EmployeeModel
+{
+    public class Employee
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string MobileNumber { get; set; }
+        public string DepartmentId { get; set; } // We are using Guid in relations
+        public bool? IsActive { get; set; }
+    }
+}
